267e4b76f867c66008d041ffbcc27166ef5fc6c3
李佳奇 <450103187@qq.com>
2021-06-07 18:46:37 +0800

    native activity : AcquireASensorManagerInstance not done

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -157,6 +157,145 @@ static int engine_init_display(struct engine* engine) {
 
 }
 
+/*
+ * display中的当前帧
+ *
+ */
+
+static void engine_draw_frame(struct engine * engine) {
+    if(engine->display == nullptr) {
+        //no display
+        return;
+    }
+
+    //屏幕填充一个颜色
+    glClearColor(((float )engine->state.x)/engine->width,engine->state.angle,
+                 ((float )engine->state.y)/engine->height,1);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    //切换buffer
+    eglSwapBuffers(engine->display,engine->surface);
+
+}
+
+/*
+ * 剔除 egl context中当前关联的display
+ */
+
+static void engine_term_display(struct engine* engine) {
+    if(engine->display != EGL_NO_DISPLAY) {
+        eglMakeCurrent(engine->display,EGL_NO_SURFACE,EGL_NO_SURFACE,EGL_NO_CONTEXT);
+        if(engine->context != EGL_NO_CONTEXT) {
+            eglDestroyContext(engine->display,engine->context);
+        }
+        if(engine->surface != EGL_NO_SURFACE) {
+            eglDestroySurface(engine->display,engine->surface);
+        }
+        eglTerminate(engine->display);
+    }
+    engine->animation = 0;
+    engine->display = EGL_NO_DISPLAY;
+    engine->context = EGL_NO_CONTEXT;
+    engine->surface = EGL_NO_SURFACE;
+}
+
+
+/*
+ * 处理下一个输入事件
+ */
+
+static int32_t engine_handle_input(struct android_app* app,AInputEvent* event) {
+    auto *engine = (struct engine*)app->userData;
+    if(AInputEvent_getType(event) == AINPUT_EVENT_TYPE_MOTION) {
+        engine->animation = 1;
+        engine->state.x = AMotionEvent_getX(event,0);
+        engine->state.y = AMotionEvent_getY(event,0);
+        return 1;
+    }
+    return 0;
+}
+
+/*
+ * 处理下一个main command
+ */
+
+static void engine_handle_cmd(struct android_app* app,int32_t cmd) {
+    auto* engine = (struct engine*)app->userData;
+    switch (cmd) {
+        case APP_CMD_SAVE_STATE:
+            //系统要求存储当前state
+            //给android_app->savedState(void* ptr)分配内存，大小等于saved_state
+            engine->app->savedState = malloc(sizeof(struct saved_state));
+            //android_app->savedState指向engine->state
+            *((struct saved_state*)engine->app->savedState) = engine->state;
+            engine->app->savedStateSize = sizeof(struct saved_state);
+            break;
+        case APP_CMD_INIT_WINDOW:
+            //窗口显示前的准备工作
+            if(engine->app->window != nullptr) {
+                engine_init_display(engine);
+                engine_draw_frame(engine);
+            }
+            break;
+        case APP_CMD_TERM_WINDOW:
+            //window 即将隐藏或者关闭，清理资源
+            engine_term_display(engine);
+            break;
+        case APP_CMD_GAINED_FOCUS:
+            //app 获取焦点时,开始监控
+            if(engine->accelerometerSensor != nullptr) {
+                ASensorEventQueue_enableSensor(engine->sensorEventQueue,
+                                               engine->accelerometerSensor);
+                //暂定为 每秒60个事件
+                ASensorEventQueue_setEventRate(engine->sensorEventQueue,
+                                               engine->accelerometerSensor,
+                                               (1000L/60)* 1000);
+            }
+            break;
+        case APP_CMD_LOST_FOCUS:
+            //app 失去焦点 停止监控
+            //可以避免浪费电
+            if(engine->accelerometerSensor != nullptr) {
+                ASensorEventQueue_disableSensor(engine->sensorEventQueue,
+                                                engine->accelerometerSensor);
+            }
+            //停滞动画
+            engine->animation = 0;
+            engine_draw_frame(engine);
+            break;
+        default:
+            break;
+
+    }
+}
+
+/*
+ * AcquireASensorManagerInstance(void)
+ *    Workaround ASensorManager_getInstance() deprecation false alarm
+ *    for Android-N and before, when compiling with NDK-r15
+ */
+
+/*
+ * Linux在<dlfnc.h>库中提供了加载和处理动态连接库的系统调用
+ * dlopen : 打开一个动态连接库，并返回一个类型为void*的handler,flag为打开模式，可选的模式有两种
+RTLD_LAZY 暂缓决定，等有需要时再解出符号
+RTLD_NOW 立即决定，返回前解除所有未决定的符号。
+dlerror : 返回dl操作的错误，若没有出现错误，则返回NUlL，否则打印错误信息
+dlsym : 查找动态链接库中的符号symbol,并返回该符号所在的地址
+dlclose : 关闭动态链接库句柄
+ */
+
+#include <dlfcn.h>
+
+//获取 传感器管理 实例
+ASensorManager * AcquireASensorManagerInstance(android_app* app) {
+    if(!app)
+        return nullptr;
+}
+
+
+
 
 
 
